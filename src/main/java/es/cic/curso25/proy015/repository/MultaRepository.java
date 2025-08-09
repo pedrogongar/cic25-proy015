e5b8844762cda3966d2426e4874b4034e6c9a021
pgongar <pgonzalezg29@educantabria.es>
2025-08-09 16:11:18 +0200

    Creación del repositorio Multa. Añadido método Optional para comprobar  si existe una multa en estado ABIERTO asociada a un vehículo determinado

---
+++ src/main/java/es/cic/curso25/proy015/repository/MultaRepository.java
@@ -0,0 +1,15 @@
+package es.cic.curso25.proy015.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import es.cic.curso25.proy015.model.Multa;
+import es.cic.curso25.proy015.model.enums.EstadoMulta;
+
+@Repository
+public interface MultaRepository extends JpaRepository<Multa, Long> {
+
+    Optional<Multa> findByVehiculoSancionado_IdAndEstado(Long vehiculoId, EstadoMulta estado);
+}
