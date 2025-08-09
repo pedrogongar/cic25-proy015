a26077f5ba11933789c94a8a6ac961f3db34dc66
pgongar <pgonzalezg29@educantabria.es>
2025-08-09 15:36:54 +0200

    Creada carpeta "enums" dentro de "model" con los enum EstadoMulta y MotivoMulta

---
+++ src/main/java/es/cic/curso25/proy015/model/enums/EstadoMulta.java
@@ -0,0 +1,6 @@
+package es.cic.curso25.proy015.model.enums;
+
+public enum EstadoMulta {
+    ABIERTA,
+    CERRADA
+}
---
+++ src/main/java/es/cic/curso25/proy015/model/enums/MotivoMulta.java
@@ -0,0 +1,5 @@
+package es.cic.curso25.proy015.model.enums;
+
+public enum MotivoMulta {
+    PLAZA_NO_ASIGNADA
+}
