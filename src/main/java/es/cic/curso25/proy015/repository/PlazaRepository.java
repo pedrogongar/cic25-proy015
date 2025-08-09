1dd28b6fe4bb425e8fd76136185cb12cd69b1f99
pgongar <pgonzalezg29@educantabria.es>
2025-08-09 16:09:05 +0200

    Creación del repositorio Multa. Añadidos métodos List<Plaza>  para encontrar plazas libres u ocupadas. Añadido método Optional para comprobar en qué plaza está aparcado un vehículo

---
+++ src/main/java/es/cic/curso25/proy015/repository/PlazaRepository.java
@@ -0,0 +1,19 @@
+package es.cic.curso25.proy015.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import es.cic.curso25.proy015.model.Plaza;
+
+@Repository
+public interface PlazaRepository extends JpaRepository<Plaza, Integer> {
+
+    List<Plaza> findByVehiculoOcupanteIsNull();
+
+    List<Plaza> findByVehiculoOcupanteIsNotNull();
+
+    Optional<Plaza> findByVehiculoOcupante_Id(Long vehiculoId);
+}
