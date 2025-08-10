552d33d387d30bf984c9b2031befb269186b98c5
pgongar <pgonzalezg29@educantabria.es>
2025-08-10 19:23:34 +0200

    Añadida excepción en caso de no encontrar el objeto en la base de datos

---
+++ src/main/java/es/cic/curso25/proy015/exception/NoEncontradoException.java
@@ -0,0 +1,8 @@
+package es.cic.curso25.proy015.exception;
+
+public class NoEncontradoException extends RuntimeException {
+
+    public NoEncontradoException(String message) {
+        super(message);
+    }
+}
