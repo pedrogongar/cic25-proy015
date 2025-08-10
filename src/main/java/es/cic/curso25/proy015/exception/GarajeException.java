428a7b7e73abc6ed8b8528216109de73d9088cd0
pgongar <pgonzalezg29@educantabria.es>
2025-08-10 19:23:57 +0200

    Añadida excepción para el service

---
+++ src/main/java/es/cic/curso25/proy015/exception/GarajeException.java
@@ -0,0 +1,8 @@
+package es.cic.curso25.proy015.exception;
+
+public class GarajeException extends RuntimeException {
+
+    public GarajeException(String message) {
+        super(message);
+    }
+}
